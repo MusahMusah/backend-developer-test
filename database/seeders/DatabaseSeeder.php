f3250f87ab65515c1c224e6afcddc69f62435b75
MusahMusah <aliyumusahmusah@gmail.com>
2024-01-21 13:11:17 +0100

    feat: added all application seeders needed to be executed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,17 +4,33 @@ namespace Database\Seeders;
 
 use App\Models\Lesson;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    protected const DEMO_USER_EMAIL = 'user@iphonephotographyschool.com';
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        $lessons = Lesson::factory()
-            ->count(20)
-            ->create();
+        User::factory([
+            'name' => 'Demo User',
+            'email' => self::DEMO_USER_EMAIL,
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+        ])
+        ->has(Lesson::factory()->count(20))
+        ->create();
+
+        $this->call([
+            CommentSeeder::class,
+            BadgeSeeder::class,
+            CommentAchievementsSeeder::class,
+            LessonAchievementsSeeder::class,
+        ]);
     }
 }
