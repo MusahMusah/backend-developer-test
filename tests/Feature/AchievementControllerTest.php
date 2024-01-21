4704df73683b340802f2e9e01696273aa6e36fb3
MusahMusah <aliyumusahmusah@gmail.com>
2024-01-21 15:38:15 +0100

    test: the application returns unlocked achievements as array of strings

--- tests/Feature/AchievementControllerTest.php
+++ tests/Feature/AchievementControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Achievement;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -26,6 +27,32 @@ class AchievementControllerTest extends TestCase
                 'current_badge',
                 'next_badge',
                 'remaing_to_unlock_next_badge',
+            ])
+            ->assertOk();
+    }
+
+    /** @test */
+    public function the_application_returns_unlocked_achievements_as_array_of_strings()
+    {
+        $user = User::factory()->create();
+        $achievements = Achievement::factory()->count(4)->create();
+        $user->userAchievements()->sync($achievements);
+
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $response
+            ->assertJsonCount(5)
+            ->assertJsonStructure([
+                'unlocked_achievements',
+                'next_available_achievements',
+                'current_badge',
+                'next_badge',
+                'remaing_to_unlock_next_badge',
+            ])
+            ->assertJsonFragment([
+                'unlocked_achievements' => $user->unlockedAchievements
+                    ->pluck('text')
+                    ->toArray()
             ])
             ->assertOk();
     }
