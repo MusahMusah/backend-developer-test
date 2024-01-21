658f96b228be07c25d52c9ef236f367d771a77b4
MusahMusah <aliyumusahmusah@gmail.com>
2024-01-21 13:20:10 +0100

    feat: create a badge for user

--- app/Listeners/BadgeUnlockedListener.php
+++ app/Listeners/BadgeUnlockedListener.php
@@ -3,24 +3,32 @@
 namespace App\Listeners;
 
 use App\Events\BadgeUnlocked;
+use App\Models\Badge;
+use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
 class BadgeUnlockedListener
 {
     /**
-     * Create the event listener.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Handle the event.
      */
     public function handle(BadgeUnlocked $event): void
     {
-        //
+        $this->createUserBadge($event->user, $event->badge_name);
+    }
+
+    private function createUserBadge(User $user, string $badgeName): void
+    {
+        // Find or create temporary badge instance
+        $badge = Badge::query()->where('name', $badgeName)->firstOrNew();
+
+        // Check if the user already has a badge
+        $existingUserBadge = $user->badge;
+
+        // Update the existing user badge or create a new one
+        $existingUserBadge
+            ? $existingUserBadge->badge()->associate($badge)->save()
+            : $user->badge()->create(['badge_id' => $badge->id]);
     }
 }
