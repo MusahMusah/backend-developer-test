a0aee0b78ae27ece0bf89e6c46ce6eb10f4c98f1
MusahMusah <aliyumusahmusah@gmail.com>
2024-01-21 13:00:41 +0100

    feat: added cascade on delete action to foreign keys

--- database/migrations/2024_01_21_115241_create_user_achievements_table.php
+++ database/migrations/2024_01_21_115241_create_user_achievements_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_achievements', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('achievement_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('achievement_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
