bfe78b38d244ca3743bd494a1c8c7dcd5b45e98a
MusahMusah <aliyumusahmusah@gmail.com>
2024-01-21 14:25:16 +0100

    test: test for unlock achievement for comment type

--- tests/Feature/AchievementTest.php
+++ tests/Feature/AchievementTest.php
@@ -2,11 +2,51 @@
 
 namespace Tests\Feature;
 
+use App\Concerns\Enums\AchievementTypeEnum;
+use App\Events\AchievementUnlocked;
+use App\Listeners\AchievementUnlockedListener;
+use App\Models\Achievement;
+use App\Models\Comment;
+use App\Models\User;
+use App\Services\AchievementService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class AchievementTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function it_unlocks_achievement_for_comment_type(): void
+    {
+        $commentAchievement = Achievement::factory()->create([
+            'type' => AchievementTypeEnum::COMMENT,
+            'count' => 1,
+        ]);
+        $user = User::factory()->create();
+        Comment::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        // Mock the event to assert later
+        Event::fake();
+
+        $service = app(AchievementService::class);
+        $service->unlockAchievement($user, AchievementTypeEnum::COMMENT);
+        $user->refresh();
+
+        Event::assertDispatched(
+            AchievementUnlocked::class,
+            fn ($event) => ($event->achievement_name === $commentAchievement->text && $event->user === $user)
+        );
+
+        Event::assertListening(
+            AchievementUnlocked::class,
+            AchievementUnlockedListener::class,
+        );
+
+        $this->assertCount(1, $user->unlockedAchievements);
+        $this->assertTrue($user->unlockedAchievements->contains($commentAchievement));
+    }
 }
